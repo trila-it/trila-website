38f46f14137512aa80d74d77731b7e96cb70fe54
andrea bonacci <andrea.bonacci20@gmail.com>
2026-06-28 14:21:36 +0200

    Smooth mobile reveals: fade only, no translateY, faster trigger

--- demo/Misa/assets/js/main.js
+++ demo/Misa/assets/js/main.js
@@ -25,7 +25,7 @@ if (ham && mob) {
 // Reveal
 const io = new IntersectionObserver(entries => {
   entries.forEach(e => { if (e.isIntersecting) { e.target.classList.add('in'); io.unobserve(e.target); } });
-}, { threshold: 0, rootMargin: '0px 0px -40px 0px' });
+}, { threshold: 0, rootMargin: '0px 0px 0px 0px' });
 document.querySelectorAll('.reveal').forEach(el => io.observe(el));
 
 // Booking bar date display
